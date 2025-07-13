1ff00cd0ec2c09c60d1f5dce03992d9ab86a97aa
Changer0001 <yilmmazburak@gmail.com>
2025-07-13 10:11:06 -0700

    Smart RAG improved

--- src/test_retriever.py
+++ src/test_retriever.py
@@ -1,11 +1,21 @@
 from myapp.retriever import retrieve
 import chromadb
 import os
+from myapp.llm_interface import ask_llm_hf
+
 CHROMA_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "src", "chroma_db"))
 client = chromadb.PersistentClient(path=CHROMA_PATH)
 
-
 if __name__ == "__main__":
-    result = retrieve("What is the refund policy?")
+    retrieved_context = retrieve("What is the refund policy?")
     print("\n🧠 Retrieved Context:\n")
-    print(result)
+    print(retrieved_context)
+
+    answer = ask_llm_hf(
+        question="What is the refund policy?",
+        history_blocks="",
+        doc_context=retrieved_context,
+        user_id=123
+    )
+
+    print("\n💬 Answer from LLM:\n", answer)
